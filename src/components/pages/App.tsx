4cb29120465d9309785f0e471c8326b44c6c068a
wongco <ginson@gmail.com>
2019-03-07 11:54:50 -0800

    update axios requests

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,4 +1,5 @@
 import 'aos/dist/aos.css';
+import axios from 'axios';
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
@@ -8,10 +9,19 @@ import Contact from '../organisms/Contact';
 import Navbar from '../organisms/Navbar';
 import Portfolio from '../organisms/Portfolio';
 
+const HACK_OR_SNOOZE_API = 'https://hack-or-snooze-api.herokuapp.com/';
+const JOBLY_FRONTEND = 'https://jobly-wongco.herokuapp.com/';
+const JOBLY_BACKEND_API = 'https://jobly-api-wongco.herokuapp.com/';
+
 const StyledApp = styled.div`
   background: linear-gradient(to bottom, white, #ffb900);
 `;
 class App extends Component {
+  public async componentDidMount() {
+    axios.get(HACK_OR_SNOOZE_API);
+    axios.get(JOBLY_FRONTEND);
+    axios.get(JOBLY_BACKEND_API);
+  }
   public render() {
     return (
       <StyledApp id="top">
