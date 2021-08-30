ed8b078993dad5de63f021b18a9ea46cc0121001
wongco <ginson@gmail.com>
2021-08-29 17:04:12 -0700

    Updates

---
+++ precache-manifest.6217d59852165cb35ddce08b4f7a0afb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c398811d8c691b8117db",
+    "url": "/static/css/main.cf1dcceb.chunk.css"
+  },
+  {
+    "revision": "c398811d8c691b8117db",
+    "url": "/static/js/main.c398811d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f24426c3f64a82138be3",
+    "url": "/static/css/2.3c83d403.chunk.css"
+  },
+  {
+    "revision": "f24426c3f64a82138be3",
+    "url": "/static/js/2.f24426c3.chunk.js"
+  },
+  {
+    "revision": "da9ade577fba3b46bbeb4ec75e3d1225",
+    "url": "/static/media/profile_pic1c.da9ade57.jpg"
+  },
+  {
+    "revision": "cabafbb90fa6848bd6d338947cf9822f",
+    "url": "/static/media/baybridge_banner2.cabafbb9.jpg"
+  },
+  {
+    "revision": "08b203bae998a7f4c99f4cb69d93e2bf",
+    "url": "/static/media/groupmuse04.08b203ba.png"
+  },
+  {
+    "revision": "c809a313bbe3d3523921df637c476f93",
+    "url": "/static/media/hackorsnooze03.c809a313.jpg"
+  },
+  {
+    "revision": "8d2833f342277b91a93c6c212c191750",
+    "url": "/static/media/jobly03.8d2833f3.jpg"
+  },
+  {
+    "revision": "0b8d3f6d6c23e893b97aa9109f7a500d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.433e6574467ddace07dbda9389375ccc.js"
+  "/precache-manifest.6217d59852165cb35ddce08b4f7a0afb.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
