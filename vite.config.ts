854c8d9358977985831d48279f30d0f495b96fbb
Ginson Wong <ginson@gmail.com>
2023-12-15 00:04:55 -0800

    initial install of stylex

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import styleX from "./vite-stylex-plugin/index.mjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), styleX()],
 });
