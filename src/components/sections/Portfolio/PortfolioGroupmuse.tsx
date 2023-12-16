bb577964fda9711b17f60e86461bfb51b221c870
Ginson Wong <ginson@gmail.com>
2023-12-16 14:49:53 -0800

    refactor groupmuse component to stylex

--- src/components/sections/Portfolio/PortfolioGroupmuse.tsx
+++ src/components/sections/Portfolio/PortfolioGroupmuse.tsx
@@ -1,45 +1,51 @@
-import styled from "styled-components";
 import ProjectInfo from "./ProjectInfo";
 import preview_groupmuse from "../../../pics/groupmuse04.png";
+import * as stylex from "@stylexjs/stylex";
 
-const StyledGroupmuseContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  border-top: 1px solid gray;
-  padding-top: 3em;
-
-  @media (max-width: 767.98px) {
-    flex-direction: column;
-  }
-`;
-
-const StyledImgContainer = styled.div`
-  display: flex;
-  align-items: flex-end;
-  height: 100%;
-  width: 25vw;
-  margin-top: 50px;
-
-  @media (max-width: 767.98px) {
-    margin-top: 0;
-    width: 70vw;
-  }
-`;
-
-const StyledImg = styled.img`
-  width: 100%;
-`;
+const styles = stylex.create({
+  groupmuseContainerStyles: {
+    display: "flex",
+    flexDirection: {
+      default: "row",
+      "@media (max-width: 767.98px)": "column",
+    },
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
+    height: "100%",
+    borderTopWidth: "1px",
+    borderTopColor: "gray",
+    borderTopStyle: "solid",
+    paddingTop: "3em",
+  },
+  imageContainerStyles: {
+    display: "flex",
+    alignItems: "flex-end",
+    height: "100%",
+    width: {
+      default: "25vw",
+      "@media (max-width: 767.98px)": "70vw",
+    },
+    marginTop: {
+      default: "50px",
+      "@media (max-width: 767.98px)": "0",
+    },
+  },
+  imageStyles: {
+    width: "100%",
+  },
+});
 
 export default function PortfolioGroupmuse() {
   return (
-    <StyledGroupmuseContainer>
-      <StyledImgContainer>
-        <StyledImg src={preview_groupmuse} alt="groupmuse preview image" />
-      </StyledImgContainer>
+    <div {...stylex.props(styles.groupmuseContainerStyles)}>
+      <div {...stylex.props(styles.imageContainerStyles)}>
+        <img
+          {...stylex.props(styles.imageStyles)}
+          src={preview_groupmuse}
+          alt="groupmuse preview image"
+        />
+      </div>
       <ProjectInfo
         tags={[
           "React Native",
@@ -51,6 +57,6 @@ export default function PortfolioGroupmuse() {
         header="Groupmuse"
         body="Refactoring the templates and dependent components for the authorization workflow enabled our agile team to create a foundation for rebuilding the react native mobile app. This was achieved using compound components, render props, and atomic design."
       />
-    </StyledGroupmuseContainer>
+    </div>
   );
 }
