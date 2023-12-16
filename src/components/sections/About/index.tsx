ecf54a6b205b1beda0e223aea24f2905322fec96
Ginson Wong <ginson@gmail.com>
2023-12-16 01:40:39 -0800

    refactor about component to stylex

--- src/components/sections/About/index.tsx
+++ src/components/sections/About/index.tsx
@@ -1,8 +1,18 @@
 import AOS from "aos";
 import { useEffect } from "react";
-import styled from "styled-components";
 import preview from "../../../pics/profile_pic1c.jpg";
 import * as stylex from "@stylexjs/stylex";
+
+const header =
+  "Behind every problem is an elegant solution waiting to be created.";
+
+const bodyPartOne = `I'm a software engineer currently based in San Francisco. Prior to this, I was a Systems\
+Administrator building technology solutions to solve critical business challenges. You can\
+now find me tackling new challenges as a full stack software engineer coding primarily in\
+JavaScript and Python.`;
+
+const bodyPartTwo = `You can also find me shooting photos and eating my way through the San Francisco Bay Area.\
+Interested in connecting with me professionally? Reach out to me on any of my contact links.`;
 
 const styles = stylex.create({
   containerStyles: {
@@ -46,62 +56,59 @@ const styles = stylex.create({
       "@media (max-width: 767.98px)": "1.25em",
     },
   },
+  aboutBodyContainerStyles: {
+    display: "flex",
+    flexDirection: {
+      default: "row",
+      "@media (max-width: 767.98px)": "column",
+    },
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imageContainerStyles: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "18.5em",
+    width: "18.5em",
+    padding: {
+      default: "5em",
+      "@media (max-width: 767.98px)": "0",
+    },
+    overflow: "hidden",
+  },
+  imageStyles: {
+    borderRadius: "50%",
+    borderWidth: "5px",
+    borderStyle: "solid",
+    borderColor: "white",
+    transform: "rotate(-2deg)",
+    objectFit: "contain",
+    width: "auto",
+    height: "250px",
+    boxShadow: "5px 5px 18px gray",
+  },
+  bodyStyles: {
+    color: "#45474c",
+    fontSize: {
+      default: "1.75vw",
+      "@media (max-width: 767.98px)": "5vw",
+    },
+    lineHeight: {
+      default: "1.75em",
+      "@media (max-width: 767.98px)": "1.5em",
+    },
+    width: {
+      default: "50%",
+      "@media (max-width: 767.98px)": "70vw",
+    },
+    fontWeight: 300,
+    padding: {
+      default: "0",
+      "@media (max-width: 767.98px)": "1em 0",
+    },
+  },
 });
-
-const StyledAboutContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-
-  @media (max-width: 767.98px) {
-    flex-direction: column;
-  }
-`;
-
-const StyledImgContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 18.5em;
-  width: 18.5em;
-  padding: 5em;
-  overflow: hidden;
-
-  @media (max-width: 767.98px) {
-    padding: 0;
-  }
-`;
-
-const StyledImg = styled.img`
-  border-radius: 50%;
-  border: 5px solid white;
-  transform: rotate(-2deg);
-  object-fit: contain;
-
-  width: auto;
-  height: 250px;
-  box-shadow: 5px 5px 18px gray;
-
-  @media (max-width: 767.98px) {
-  }
-`;
-
-const StyledBody = styled.div`
-  color: #45474c;
-  font-size: 1.75vw;
-  line-height: 1.75em;
-  width: 50%;
-  font-weight: 300;
-  padding: 0;
-
-  @media (max-width: 767.98px) {
-    padding: 1em 0;
-    font-size: 5vw;
-    line-height: 1.5em;
-    width: 70vw;
-  }
-`;
 
 export default function About() {
   useEffect(() => {
@@ -113,30 +120,28 @@ export default function About() {
   return (
     <section {...stylex.props(styles.containerStyles)} id="about">
       <div {...stylex.props(styles.boxStyles)}>
-        <p {...stylex.props(styles.aboutHeaderStyles)}>
-          Behind every problem is an elegant solution waiting to be created.
-        </p>
+        <p {...stylex.props(styles.aboutHeaderStyles)}>{header}</p>
       </div>
-      <StyledAboutContainer>
-        <StyledImgContainer
+      <div {...stylex.props(styles.aboutBodyContainerStyles)}>
+        <div
+          {...stylex.props(styles.imageContainerStyles)}
           data-aos="fade-right"
           data-aos-delay="200"
           data-aos-easing="ease-in-sine"
         >
-          <StyledImg src={preview} alt="headshot" />
-        </StyledImgContainer>
-        <StyledBody>
-          {`I'm a software engineer currently based in San Francisco. Prior to this, I was a Systems
-          Administrator building technology solutions to solve critical business challenges. You can
-          now find me tackling new challenges as a full stack software engineer coding primarily in
-          JavaScript and Python.`}
+          <img
+            {...stylex.props(styles.imageStyles)}
+            src={preview}
+            alt="headshot"
+          />
+        </div>
+        <div {...stylex.props(styles.bodyStyles)}>
+          {bodyPartOne}
           <br />
           <br />
-          {`You can also find me shooting photos and eating my way through the San Francisco Bay Area.
-          Interested in connecting with me professionally? Reach out to me on any of my contact
-          links.`}
-        </StyledBody>
-      </StyledAboutContainer>
+          {bodyPartTwo}
+        </div>
+      </div>
     </section>
   );
 }
