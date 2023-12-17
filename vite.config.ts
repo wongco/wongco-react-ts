7d9cd82bd321503500f4800fc088f32a91621811
Ginson Wong <ginson@gmail.com>
2023-12-16 16:56:36 -0800

    move plugin to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import styleX from "./vite-stylex-plugin/index.mjs";
+import styleX from "./vite-stylex-plugin.mjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
