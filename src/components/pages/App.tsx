130023da61ed7cbedc223770359a01e4efd66c7c
Ginson Wong <ginson@gmail.com>
2023-12-16 16:32:51 -0800

    refactor App component to styleX

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,23 +1,25 @@
 import "aos/dist/aos.css";
-import styled from "styled-components";
 import About from "../sections/About";
 import Banner from "../sections/Banner";
 import Contact from "../sections/Contact";
 import Navbar from "../sections/Navbar";
 import Portfolio from "../sections/Portfolio";
+import * as stylex from "@stylexjs/stylex";
 
-const StyledApp = styled.div`
-  background: linear-gradient(to bottom, white, #ffb900);
-`;
+const styles = stylex.create({
+  appStyles: {
+    backgroundImage: "linear-gradient(to bottom, white, #ffb900)",
+  },
+});
 
 export default function App() {
   return (
-    <StyledApp id="top">
+    <div {...stylex.props(styles.appStyles)} id="top">
       <Navbar />
       <Banner />
       <Portfolio />
       <About />
       <Contact />
-    </StyledApp>
+    </div>
   );
 }
